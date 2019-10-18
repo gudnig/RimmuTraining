53e8522fb69a2ae823e42c1a2db67322029ca760
Guðni Garðarsson <gudni@adventures.is>
2019-10-18 16:36:35 +0000

    Connecting secret vault

--- RimmuTraining.WebApp/Program.cs
+++ RimmuTraining.WebApp/Program.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -18,11 +21,26 @@ namespace RimmuTraining.WebApp
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            
+            return Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((context, config) =>
+            {
+                if (context.HostingEnvironment.IsDevelopment())
+                {
+
+                }
+                else
+                {
+                    var tokenProvider = new AzureServiceTokenProvider();
+                    //Create the Key Vault client
+                    var kvClient = new KeyVaultClient((authority, resource, scope) => tokenProvider.KeyVaultTokenCallback(authority, resource, scope));
+                    config.AddAzureKeyVault("https://rimmukeys.vault.azure.net/", kvClient, new DefaultKeyVaultSecretManager());
+                }
+            }).ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
